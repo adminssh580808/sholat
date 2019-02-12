e6324095de97a447f592f5e926dd0bb2f00cdc8d
adminssh58 <47466957+adminssh58@users.noreply.github.com>
2019-02-12 23:43:03 +0700

    Update sholat.py

--- sholat.py
+++ sholat.py
@@ -8,7 +8,7 @@ def clear():
     return os.system("cls") if (platform.system() == 'Windows') else os.system("clear")
 
 clear()
-location = input("Ketik Lokasi Anda, Contoh: jakarta??"+"\n")
+location = input("Ketik Lokasi Anda Lalu ENTER, Contoh: jakarta??"+"\n")
 req = requests.get('https://time.siswadi.com/pray/'+location)
 st = json.loads(req.content)
 dat = (st["data"])
@@ -18,10 +18,13 @@ asar = (dat["Asr"])
 magrib = (dat["Maghrib"])
 isya = (dat["Isha"])
 
-print ("Info Time "+str(time.strftime('%a, %d %B %Y |%H/%M/%S')))
+echo "************************************************"
+print ("Info Time "+str(time.strftime('%a, %d %B %Y | %H:%M:%S')))
 print("Jadwal Sholat Hari Ini Di "+location)
-print ("Subuh: "+subuh+"\n")
-print ("Dzuhur: "+duhur+"\n")
-print ("Ashar: "+asar+"\n")
-print ("Mahgrib: "+magrib+"\n")
-print ("Isya: "+isya+"\n")
+echo "************************************************"
+echo " "
+print ("Subuh   : "+subuh+"\n")
+print ("Dzuhur  : "+duhur+"\n")
+print ("Ashar   : "+asar+"\n")
+print ("Mahgrib : "+magrib+"\n")
+print ("Isya    : "+isya+"\n")
