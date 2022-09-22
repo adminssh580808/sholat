3e9e0f9524825abb8c256413a591808e0d2a1401
adminssh580808 <47466957+adminssh580808@users.noreply.github.com>
2022-09-22 15:15:15 +0700

    Update sholat.py

--- sholat.py
+++ sholat.py
@@ -8,7 +8,7 @@ def clear():
     return os.system("cls") if (platform.system() == 'Windows') else os.system("clear")
 
 clear()
-location = input("Ketik Lokasi Anda Lalu ENTER, Contoh: jakarta??"+"\n")
+location = input("Ketik Lokasi Anda Lalu Klik ENTER, Contoh: jakarta??"+"\n")
 req = requests.get('https://time.siswadi.com/pray/'+location)
 st = json.loads(req.content)
 dat = (st["data"])
@@ -19,7 +19,7 @@ magrib = (dat["Maghrib"])
 isya = (dat["Isha"])
 
 print ("Time Info: "+str(time.strftime('%a, %d %B %Y | %H:%M:%S')) "WIB")
-print("Jadwal Sholat Update Hari Ini Di "+location)
+print("Jadwal Sholat Uptodate Hari Ini Di "+location)
 print ("Subuh   : "+subuh+"\n")
 print ("Dzuhur  : "+duhur+"\n")
 print ("Ashar   : "+asar+"\n")
